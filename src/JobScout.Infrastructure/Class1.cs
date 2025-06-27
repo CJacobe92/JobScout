c21a04e8eca608df85c5c08518360df20b8512bf
CJacobe92 <jacobe.christian92@gmail.com>
2025-06-27 16:08:37 -0700

    Testing

---
+++ src/JobScout.Infrastructure/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobScout.Infrastructure
+{
+    internal class Class1
+    {
+    }
+}
